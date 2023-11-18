24de569943d04733e831a2bcaeee892f0fcdb9b6
mmk <myat1999kyaw@gmail.com>
2023-11-18 16:57:05 +0630

    add search feature

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -3,7 +3,11 @@ import { deleteContact } from "../contacts";
 
 
 export async function action({params}){
-    throw new Error("Oh dang!");
-    await deleteContact(params.contactId);
-    return redirect("/");
+    
+    if(await deleteContact(params.contactId)){
+        return redirect("/");
+    }else{
+        throw new Error("Oh dang!");
+    }
+    
 }
